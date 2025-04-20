33e2720c95cf2ca1321a9ab099dde7fa88737615
Namami Verma <Namami.24BCS10349@sst.scaler.com>
2025-04-20 17:58:32 +0530

    Context API for Exercises

---
+++ src/ContextAPI/ExercisesContext.jsx
@@ -0,0 +1,62 @@
+import React, { createContext, useState, useContext, Suspense, lazy } from 'react';
+
+const ExercisesContext = createContext();
+
+// Custom hook to use the context
+export const useExercises = () => {
+    return useContext(ExercisesContext);
+};
+
+const Exercises = lazy(() => import('../components/Exercises'))
+
+// Create the provider component
+//children: Exercises Component
+export const ExercisesProvider = ({ children }) => {
+    const [exerciseSuggestions, setExerciseSuggestions] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [selectedExercise, setSelectedExercise] = useState('');  // Managing the selectedExercise
+
+    // Fetching exercises based on the body part
+    const fetchExercises = async (type) => {
+        const apiKey = 'cc65018371msh27356ce58896615p1ebc2djsn88b4e9e7967b';
+        const url = `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${type}?limit=8`;
+
+        setLoading(true);
+        try {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'X-RapidAPI-Key': apiKey,
+                    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+                },
+            });
+
+            const data = await response.json();
+            if (data.length > 0) {
+                setExerciseSuggestions(data);
+            } else {
+                setExerciseSuggestions([]);
+            }
+        } catch (error) {
+            console.error('Error fetching exercise data:', error);
+            setExerciseSuggestions([]);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <ExercisesContext.Provider
+            value={{
+                exerciseSuggestions,
+                selectedExercise,
+                setSelectedExercise,
+                fetchExercises,
+            }}
+        >
+            <Suspense fallback={<div>Loading...</div>}>
+                {children}
+            </Suspense>
+        </ExercisesContext.Provider>
+    );
+};
