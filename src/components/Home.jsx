702c620d07c3a6fe8faec9c1ea51114c5dd5c1a7
Namami Verma <Namami.24BCS10349@sst.scaler.com>
2025-04-20 16:35:17 +0530

    Updated Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,20 @@
-//About or Home Page
-
+//Home Page that describes details about the App
 import React from "react";
-import { Link } from "react-router-dom";
 
 function Home() {
+
     return (
         <div className="bg-white min-h-screen px-6 py-4">
             <h1 className="text-5xl font-extrabold text-center text-black mb-6">
                 Welcome to WelloCity
             </h1>
 
-            <section className="mb-12">
+            <section className="mb-12 mt-10">
                 <h2 className="text-4xl font-semibold text-center text-black mb-4">
                     About WelloCity
                 </h2>
                 <p className="text-lg text-center text-gray-700 mb-4">
-                    Welcome to **WelloCity**, your personal health and wellness
+                    Welcome to <strong>WelloCity</strong>, your personal health and wellness
                     companion! Our mission is to empower you to live a healthier,
                     happier life by providing you with the tools, resources, and
                     support you need to achieve your fitness goals.
@@ -31,8 +30,8 @@ function Home() {
 
                 <ul className="list-disc pl-6 text-lg text-gray-700 mb-6">
                     <li>Nutrition Guidance</li>
-                    <li>Exercise Tracking</li>
-                    <li>Fitness Challenges</li>
+                    <li>Exercise Guidance</li>
+                    <li>Fitness Tracking</li>
                 </ul>
 
                 <p className="text-lg text-center text-gray-700 mb-4">
@@ -47,7 +46,7 @@ function Home() {
 
                 <p className="text-lg text-center text-gray-700 mb-4">
                     <span className="font-bold">Legal Disclaimer:</span>
-                    Please note that the information provided in this app is for informational purposes only
+                    &nbsp;Please note that the information provided in this app is for informational purposes only
                     and should not be used as a substitute for professional medical advice.
                     Always consult with a healthcare professional before starting any new fitness or nutrition program.
                 </p>
